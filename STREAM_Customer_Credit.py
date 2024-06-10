54fa82f8c1fce36e847622832aded5b462098f75
cantikamerita <161821354+cantikamerita@users.noreply.github.com>
2024-06-10 21:58:58 +0700

    Update STREAM_Customer_Credit.py

--- STREAM_Customer_Credit.py
+++ STREAM_Customer_Credit.py
@@ -13,37 +13,37 @@ else:
 # judul web
 st.title('Prediksi Customer Credit')
 
-checking_status = st.text_input('Input Checking Status ( 0.0 = Checking ; 1.0 = No Checking)')
+checking_status = st.text_input('1. Input Checking Status ( 0.0 = Checking ; 1.0 = No Checking)')
 
-duration = st.text_input('Input Duration')
+duration = st.text_input('2. Input Duration')
 
-credit_history = st.text_input('Input Credit History ( 0.0 = all paid , 1.0 = existing paid ,2.0 = delayed previously , 3.0 = critical/other existing credit)')
+credit_history = st.text_input('3. Input Credit History ( 0.0 = all paid , 1.0 = existing paid ,2.0 = delayed previously , 3.0 = critical/other existing credit)')
 
-credit_amount = st.text_input('Input Credit Amount')
+credit_amount = st.text_input('4. Input Credit Amount')
 
-savings_status = st.text_input('Input Saving Status (  0.0 = no known savings, 1.1 = there are known savings )')
+savings_status = st.text_input(' 5. Input Saving Status (  0.0 = no known savings, 1.1 = there are known savings )')
 
-employment = st.text_input('Input Employment ( 0.0 = unemployed , 1.0 = working  )')
+employment = st.text_input( '6. Input Employment ( 0.0 = unemployed , 1.0 = working  )')
 
-installment_commitment = st.text_input('Input Installment Comitment')
+installment_commitment = st.text_input('7. Input Installment Comitment')
 
-personal_status = st.text_input('Input Personal Status ( 0.0 = married , 1.0 = single )')
+personal_status = st.text_input('8. Input Personal Status ( 0.0 = married , 1.0 = single )')
 
-other_parties = st.text_input('Input Other Parties ( 0.0 = co applicant ,1.0 = guarantor, 2.0 = none )')
+other_parties = st.text_input('9. Input Other Parties ( 0.0 = co applicant ,1.0 = guarantor, 2.0 = none )')
 
-residence_since = st.text_input('Input Residence Since')
+residence_since = st.text_input('10. Input Residence Since')
 
-age = st.text_input('Input Age')
+age = st.text_input('11. Input Age')
 
-housing = st.text_input('Input Housing ( 0.0 = for free , 1.0 = own )')
+housing = st.text_input('12. Input Housing ( 0.0 = for free , 1.0 = own )')
 
-existing_credits = st.text_input('Input Existing Credits')
+existing_credits = st.text_input('13. Input Existing Credits')
 
-job = st.text_input('Input Job ( 0.0 = skilled , 1.0 = unskilled )')
+job = st.text_input('14. Input Job ( 0.0 = skilled , 1.0 = unskilled )')
 
-num_dependents = st.text_input('Input Num Dependents')
+num_dependents = st.text_input('15. Input Num Dependents')
 
-own_telephone = st.text_input('Input Own Telephone ( 0.0 = none , 1.0 = yes)')
+own_telephone = st.text_input('16. Input Own Telephone ( 0.0 = none , 1.0 = yes)')
 
 # Code untuk prediksi
 CustomerCredit_prediksi = ''
